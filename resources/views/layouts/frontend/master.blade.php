89df67ed7a76e6aa9c90dd24e8a8c1f9210b0377
Muhammad Kaddafi Suyatno <kaddafi.suyatno@bps.go.id>
2019-12-17 14:14:24 +0700

    auth rev

--- resources/views/layouts/frontend/master.blade.php
+++ resources/views/layouts/frontend/master.blade.php
@@ -48,7 +48,7 @@
       <!-- Notifications Dropdown Menu -->
       <li class="nav-item dropdown">
         <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="fas fa-user-cog"></i>  {{ Auth::user()->name }}
+        {{ Auth::user()->nama }} <i class="fas fa-user-cog"></i>
         </a>
         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
           <div class="dropdown-divider"></div>
